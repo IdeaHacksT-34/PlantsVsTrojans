290bb7e8e0952dbcfff47becd9e3a54adecb446e
Vinh Nguyen <vinhsnguyen86@gmail.com>
2023-01-21 20:24:52 -0800

    all same background

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ philip = {
     'char': quick_load(cwd + str(Path("/Game Files/Images/Leads/philip.png"))),
     'obstacle': quick_load(cwd + str(Path("/Game Files/Images/Obstacles/philip_obstacle.png"))),
     'catch': quick_load(cwd + str(Path("/Game Files/Images/Catch/philip_catch.png"))),
-    'background': quick_load(cwd + str(Path("/Game Files/Images/Backgrounds/philip_bg.png"))),
+    'background': quick_load(cwd + str(Path("/Game Files/Images/Backgrounds/royce_bg.png"))),
     'inactive': quick_load(cwd + str(Path("/Game Files/Images/Borders/philip_border_inactive.png"))),
     'active': quick_load(cwd + str(Path("/Game Files/Images/Borders/philip_border_active.png")))
 }
@@ -87,7 +87,7 @@ jessica = {
     'char': quick_load(cwd + str(Path("/Game Files/Images/Leads/jessica.png"))),
     'obstacle': quick_load(cwd + str(Path("/Game Files/Images/Obstacles/jessica_obstacle.png"))),
     'catch': quick_load(cwd + str(Path("/Game Files/Images/Catch/jessica_catch.png"))),
-    'background': quick_load(cwd + str(Path("/Game Files/Images/Backgrounds/jessica_bg.png"))),
+    'background': quick_load(cwd + str(Path("/Game Files/Images/Backgrounds/royce_bg.png"))),
     'inactive': quick_load(cwd + str(Path("/Game Files/Images/Borders/jessica_border_inactive.png"))),
     'active': quick_load(cwd + str(Path("/Game Files/Images/Borders/jessica_border_active.png")))
 }
@@ -96,7 +96,7 @@ andrea = {
     'char': quick_load(cwd + str(Path("/Game Files/Images/Leads/andrea.png"))),
     'obstacle': quick_load(cwd + str(Path("/Game Files/Images/Obstacles/andrea_obstacle.png"))),
     'catch': quick_load(cwd + str(Path("/Game Files/Images/Catch/andrea_catch.png"))),
-    'background': quick_load(cwd + str(Path("/Game Files/Images/Backgrounds/andrea_bg.png"))),
+    'background': quick_load(cwd + str(Path("/Game Files/Images/Backgrounds/royce_bg.png"))),
     'inactive': quick_load(cwd + str(Path("/Game Files/Images/Borders/andrea_border_inactive.png"))),
     'active': quick_load(cwd + str(Path("/Game Files/Images/Borders/andrea_border_active.png")))
 }
@@ -105,7 +105,7 @@ eric = {
     'char': quick_load(cwd + str(Path("/Game Files/Images/Leads/eric.png"))),
     'obstacle': quick_load(cwd + str(Path("/Game Files/Images/Obstacles/eric_obstacle.png"))),
     'catch': quick_load(cwd + str(Path("/Game Files/Images/Catch/eric_catch.png"))),
-    'background': quick_load(cwd + str(Path("/Game Files/Images/Backgrounds/eric_bg.png"))),
+    'background': quick_load(cwd + str(Path("/Game Files/Images/Backgrounds/royce_bg.png"))),
     'inactive': quick_load(cwd + str(Path("/Game Files/Images/Borders/eric_border_inactive.png"))),
     'active': quick_load(cwd + str(Path("/Game Files/Images/Borders/eric_border_active.png")))
 }
